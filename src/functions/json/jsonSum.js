c757e4939ed764ec3d46ae237e8b1ed227462dc9
Mlad <109038115+eolthar@users.noreply.github.com>
2025-12-19 00:32:42 +0300

    Add files via upload

---
+++ src/functions/json/jsonSum.js
@@ -0,0 +1,25 @@
+const { NativeFunction, ArgType } = require("@tryforge/forgescript");
+
+exports.default = new NativeFunction({
+    name: "$jsonSum",
+    description: "Adds a number to a JSON key",
+    version: "1.0.0",
+    output: ArgType.Boolean,
+    brackets: true,
+    unwrap: true,
+    args: [
+        {
+            name: "keys;value",
+            description: "Keys to traverse, last element is the target value",
+            type: ArgType.String,
+            required: true,
+            rest: true
+        }
+    ],
+    execute(ctx, [keys]) {
+        const val = +keys.pop();
+        const num = ctx.getEnvironmentKey(...keys);
+        const nex = (+num || 0) + val;
+        return this.success(ctx.traverseAddEnvironmentKey(typeof num === "string" ? nex + "" : nex, ...keys));
+    }
+});
